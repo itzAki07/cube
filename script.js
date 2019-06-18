76797cfc1ea00631ab74a2fd515c0358fdf17a45
itzAki07 <valdimor@gmail.com>
2019-06-18 14:39:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,8 +10,7 @@ function handleOrientation(event) {
     var x = event.beta;  // In degree in the range [-180,180]
     var y = event.gamma; // In degree in the range [-90,90]
 
-    output.innerHTML = "beta : " + x + "\n";
-    output.innerHTML += "gamma: " + y + "\n";
+
 
     // Because we don't want to have the device upside down
     // We constrain the x value to the range [-90,90]
